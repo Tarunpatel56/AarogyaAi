523ba981d8893de7a266417f0c9cadc657db4a06
Tarunpatel56 <tarunpatel0196@gmail.com>
2025-10-30 21:39:57 +0530

    add project

--- lib/login/login_screen.dart
+++ lib/login/login_screen.dart
@@ -15,105 +15,135 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-
-          children: [
-            SizedBox(height: MediaQuery.sizeOf(context).height * 0.05),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: MediaQuery.sizeOf(context).height * 0.25,
-                  width: MediaQuery.sizeOf(context).width * 0.5,
-                  child: Image.asset("assets/logo3.png"),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+        
+            children: [
+              SizedBox(height: MediaQuery.sizeOf(context).height * 0.05),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: MediaQuery.sizeOf(context).height * 0.25,
+                    width: MediaQuery.sizeOf(context).width * 0.5,
+                    child: Image.asset("assets/logo3.png"),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: MediaQuery.sizeOf(context).height * 0.01),
-
-            Text(
-              "Welcome back",
-              style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: MediaQuery.sizeOf(context).height * 0.02),
-            Form(
-              key: controller.formkey,
-              child: Column(
-                children: [
-                  Card(
-                    elevation: 5,
-                    child: TextFormField(
-                      // validator: (value) {
-                      //   if (value!.contains("@gmail.com") == false)
-                      //     return "enter your valid email";
-                      // },
-                      controller: controller.emailcontroller,
-
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.grey.shade700,
-                            width: 15,
+              SizedBox(height: MediaQuery.sizeOf(context).height * 0.01),
+        
+              Text(
+                "Welcome back",
+                style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: MediaQuery.sizeOf(context).height * 0.02),
+              Form(
+                key: controller.formkey,
+                child: Column(
+                  children: [
+                    Card(
+                      elevation: 5,
+                      child: TextFormField(
+                        // validator: (value) {
+                        //   if (value!.contains("@gmail.com") == false)
+                        //     return "enter your valid email";
+                        // },
+                        controller: controller.emailcontroller,
+        
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.grey.shade700,
+                              width: 15,
+                            ),
                           ),
+                          errorBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.red),
+                          ),
+        
+                          hintText: "Enter your email",
                         ),
-                        errorBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.red),
+                      ),
+                    ),
+                    SizedBox(height: MediaQuery.sizeOf(context).height * 0.01),
+                    Material(
+                      elevation: 10, // 👈 controls how high it looks
+                      shadowColor: Colors.black,
+                      borderRadius: BorderRadius.circular(12),
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value!.length < 8 == false) {
+                            return "enter your vaild password";
+                          }
+                        },
+                        controller: controller.passwordcontroller,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: "Password",
                         ),
-
-                        hintText: "Enter your email",
                       ),
                     ),
-                  ),
-                  SizedBox(height: MediaQuery.sizeOf(context).height * 0.01),
-                  Material(
-                    elevation: 10, // 👈 controls how high it looks
-                    shadowColor: Colors.black,
-                    borderRadius: BorderRadius.circular(12),
-                    child: TextFormField(
-                      validator: (value) {
-                        if (value!.length < 8 == false) {
-                          return "enter your vaild password";
-                        }
-                      },
-                      controller: controller.passwordcontroller,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: "Password",
+                    SizedBox(height: MediaQuery.sizeOf(context).height * 0.03),
+                    SizedBox(
+                      height: MediaQuery.sizeOf(context).height * 0.06,
+                      width: MediaQuery.sizeOf(context).width * 0.8,
+        
+                      child: ElevatedButton(
+                        onPressed: () {
+                          controller.onlogin();
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blueAccent.shade700,
+                        ),
+                        child: Text(
+                          "Login",
+                          style: TextStyle(fontSize: 18, color: Colors.white),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: MediaQuery.sizeOf(context).height * 0.03),
-                  SizedBox(
-                    height: MediaQuery.sizeOf(context).height * 0.06,
-                    width: MediaQuery.sizeOf(context).width * 0.8,
-
-                    child: ElevatedButton(
-                      onPressed: () {
-                        controller.onlogin();
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blueAccent.shade700,
-                      ),
-                      child: Text(
-                        "Login",
-                        style: TextStyle(fontSize: 18, color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: MediaQuery.sizeOf(context).height * 0.03),
-            InkWell(
-              onTap: () {
-                Get.to(ForgotScreen());
-              },
-              child: RichText(
+              SizedBox(height: MediaQuery.sizeOf(context).height * 0.03),
+              InkWell(
+                onTap: () {
+                  Get.to(ForgotScreen());
+                },
+                child: RichText(
+                  text: TextSpan(
+                    text: "Forget Password",
+                    style: TextStyle(
+                      color: Colors.blueAccent.shade700,
+                      decoration: TextDecoration.underline,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+        
+              InkWell(
+                onTap: () {
+                  Get.to(RegisterPage());
+                },
+                child: RichText(
+                  text: TextSpan(
+                    text: "Create Account",
+                    style: TextStyle(
+                      color: Colors.blueAccent.shade700,
+                      decoration: TextDecoration.underline,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: MediaQuery.sizeOf(context).height * 0.05),
+        
+              RichText(
                 text: TextSpan(
-                  text: "Forget Password",
+                  text: "Already have account?Login",
                   style: TextStyle(
                     color: Colors.blueAccent.shade700,
                     decoration: TextDecoration.underline,
@@ -121,43 +151,15 @@ class LoginScreen extends StatelessWidget {
                   ),
                 ),
               ),
-            ),
-
-            InkWell(
-              onTap: () {
-                Get.to(RegisterPage());
-              },
-              child: RichText(
-                text: TextSpan(
-                  text: "Create Account",
-                  style: TextStyle(
-                    color: Colors.blueAccent.shade700,
-                    decoration: TextDecoration.underline,
-                    fontSize: 18,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: MediaQuery.sizeOf(context).height * 0.05),
-
-            RichText(
-              text: TextSpan(
-                text: "Already have account?Login",
-                style: TextStyle(
-                  color: Colors.blueAccent.shade700,
-                  decoration: TextDecoration.underline,
-                  fontSize: 18,
-                ),
-              ),
-            ),
-
-            // SizedBox(height: MediaQuery.sizeOf(context).height*0.06,
-            //    width: MediaQuery.sizeOf(context).width*0.8,
-            //   child: OutlinedButton.icon(onPressed: (){}, label: Text("Continue with Google"),icon: SizedBox(width: MediaQuery.sizeOf(context).width*0.1,
-            //   height: MediaQuery.sizeOf(context).width*0.1,
-            //     child: Image.asset("assets/google.png")),iconAlignment: IconAlignment.start,),
-            // )
-          ],
+        
+              // SizedBox(height: MediaQuery.sizeOf(context).height*0.06,
+              //    width: MediaQuery.sizeOf(context).width*0.8,
+              //   child: OutlinedButton.icon(onPressed: (){}, label: Text("Continue with Google"),icon: SizedBox(width: MediaQuery.sizeOf(context).width*0.1,
+              //   height: MediaQuery.sizeOf(context).width*0.1,
+              //     child: Image.asset("assets/google.png")),iconAlignment: IconAlignment.start,),
+              // )
+            ],
+          ),
         ),
       ),
     );
--- lib/login/register_page.dart
+++ lib/login/register_page.dart
@@ -12,133 +12,135 @@ class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: MediaQuery.sizeOf(context).height * 0.25,
-                  width: MediaQuery.sizeOf(context).width * 0.5,
-                  child: Image.asset("assets/logo3.png"),
-                ),
-              ),
-            ),
-            Text(
-              "Create Your Account",
-              style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: MediaQuery.sizeOf(context).height * 0.02),
-            Card(
-              elevation: 8,
-              child: TextFormField(
-                controller: controller.emailcontroller,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: "Enter your email",
-                ),
-              ),
-            ),
-            SizedBox(height: MediaQuery.sizeOf(context).height * 0.01),
-            Card(
-              elevation: 8,
-              child: TextFormField(
-                controller: controller.passwordcontroller,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: "Enter your Password",
-                ),
-              ),
-            ),
-            SizedBox(height: MediaQuery.sizeOf(context).height * 0.01),
-            Card(
-              elevation: 8,
-              child: TextFormField(
-                controller: controller.confromcontroller,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: "Enter your Conform Password",
-                ),
-              ),
-            ),
-            SizedBox(height: MediaQuery.sizeOf(context).height * 0.03),
-            SizedBox(
-              height: MediaQuery.sizeOf(context).height * 0.06,
-              width: MediaQuery.sizeOf(context).width * 0.8,
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blueAccent.shade700,
-                ),
-                child: Text(
-                  "Sign up",
-                  style: TextStyle(color: Colors.white, fontSize: 18),
-                ),
-              ),
-            ),
-            SizedBox(height: MediaQuery.sizeOf(context).height * 0.03),
-
-            SizedBox(
-              height: MediaQuery.sizeOf(context).height * 0.06,
-              width: MediaQuery.sizeOf(context).width * 0.8,
-              child: OutlinedButton.icon(
-                onPressed: () {},
-                style: OutlinedButton.styleFrom(
-                  side: BorderSide(color: Colors.blueAccent.shade700, width: 3),
-                ),
-                label: Text(
-                  "Continue with Google",
-                  style: TextStyle(fontSize: 18),
-                ),
-                icon: SizedBox(
-                  height: MediaQuery.sizeOf(context).height * 0.08,
-                  width: MediaQuery.sizeOf(context).width * 0.08,
-                  child: Image.asset("assets/google.png"),
-                ),
-                iconAlignment: IconAlignment.start,
-              ),
-            ),
-            SizedBox(height: MediaQuery.sizeOf(context).height * 0.02),
-
-            SizedBox(
-              height: MediaQuery.sizeOf(context).height * 0.06,
-              width: MediaQuery.sizeOf(context).width * 0.8,
-              child: OutlinedButton.icon(
-                onPressed: () {},
-                style: OutlinedButton.styleFrom(
-                  side: BorderSide(color: Colors.blueAccent.shade700, width: 3),
-                ),
-                label: Text(
-                  "Continue with Apple",
-                  style: TextStyle(fontSize: 18),
-                ),
-                icon: SizedBox(
-                  height: MediaQuery.sizeOf(context).height * 0.08,
-                  width: MediaQuery.sizeOf(context).width * 0.08,
-                  child: Icon(Icons.apple, size: 35),
-                ),
-                iconAlignment: IconAlignment.start,
-              ),
-            ),
-            SizedBox(height: MediaQuery.sizeOf(context).height * 0.07),
-            InkWell(
-              onTap: () {
-                Get.to(BottomBar());
-              },
-              child: RichText(
-                text: TextSpan(
-                  text: "Already have account?Login",
-                  style: TextStyle(
-                    color: Colors.blueAccent.shade700,
-                    decoration: TextDecoration.underline,
-                    fontSize: 18,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: MediaQuery.sizeOf(context).height * 0.25,
+                    width: MediaQuery.sizeOf(context).width * 0.5,
+                    child: Image.asset("assets/logo3.png"),
                   ),
                 ),
               ),
-            ),
-          ],
+              Text(
+                "Create Your Account",
+                style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: MediaQuery.sizeOf(context).height * 0.02),
+              Card(
+                elevation: 8,
+                child: TextFormField(
+                  controller: controller.emailcontroller,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Enter your email",
+                  ),
+                ),
+              ),
+              SizedBox(height: MediaQuery.sizeOf(context).height * 0.01),
+              Card(
+                elevation: 8,
+                child: TextFormField(
+                  controller: controller.passwordcontroller,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Enter your Password",
+                  ),
+                ),
+              ),
+              SizedBox(height: MediaQuery.sizeOf(context).height * 0.01),
+              Card(
+                elevation: 8,
+                child: TextFormField(
+                  controller: controller.confromcontroller,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Enter your Conform Password",
+                  ),
+                ),
+              ),
+              SizedBox(height: MediaQuery.sizeOf(context).height * 0.03),
+              SizedBox(
+                height: MediaQuery.sizeOf(context).height * 0.06,
+                width: MediaQuery.sizeOf(context).width * 0.8,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueAccent.shade700,
+                  ),
+                  child: Text(
+                    "Sign up",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+              ),
+              SizedBox(height: MediaQuery.sizeOf(context).height * 0.03),
+        
+              SizedBox(
+                height: MediaQuery.sizeOf(context).height * 0.06,
+                width: MediaQuery.sizeOf(context).width * 0.8,
+                child: OutlinedButton.icon(
+                  onPressed: () {},
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(color: Colors.blueAccent.shade700, width: 3),
+                  ),
+                  label: Text(
+                    "Continue with Google",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  icon: SizedBox(
+                    height: MediaQuery.sizeOf(context).height * 0.08,
+                    width: MediaQuery.sizeOf(context).width * 0.08,
+                    child: Image.asset("assets/google.png"),
+                  ),
+                  iconAlignment: IconAlignment.start,
+                ),
+              ),
+              SizedBox(height: MediaQuery.sizeOf(context).height * 0.02),
+        
+              SizedBox(
+                height: MediaQuery.sizeOf(context).height * 0.06,
+                width: MediaQuery.sizeOf(context).width * 0.8,
+                child: OutlinedButton.icon(
+                  onPressed: () {},
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(color: Colors.blueAccent.shade700, width: 3),
+                  ),
+                  label: Text(
+                    "Continue with Apple",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  icon: SizedBox(
+                    height: MediaQuery.sizeOf(context).height * 0.08,
+                    width: MediaQuery.sizeOf(context).width * 0.08,
+                    child: Icon(Icons.apple, size: 35),
+                  ),
+                  iconAlignment: IconAlignment.start,
+                ),
+              ),
+              SizedBox(height: MediaQuery.sizeOf(context).height * 0.07),
+              InkWell(
+                onTap: () {
+                  Get.to(BottomBar());
+                },
+                child: RichText(
+                  text: TextSpan(
+                    text: "Already have account?Login",
+                    style: TextStyle(
+                      color: Colors.blueAccent.shade700,
+                      decoration: TextDecoration.underline,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
